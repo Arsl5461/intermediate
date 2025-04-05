8f6149179e35cdca464a31387dd194c5b0342255
Your Name <your.email@example.com>
2025-04-05 14:22:06 +0500

    token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -50,7 +50,7 @@ export const login = async(req,res)=>{
             return res.status(400).json({status:false, message:"User not exist"})
         }
 
-        const token = await user.getAuthToken()
+        const token =  user.getAuthToken()
 
     
         res.status(200).json({status:true, message :"User logged in successfully",token})
